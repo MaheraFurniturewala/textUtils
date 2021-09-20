17f78966aafefc371d42efb85449999e2c6771d0
Mahera Furniturewala <maherafurniturewala1111@gmail.com>
2021-09-20 19:30:33 +0530

    add text form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import Navbar from './components/Navbar.js';
+import TextForm from './components/TextForm.js';
 function App() {
   return (
    <>
    {/* here the title is a prop that is being passed*/}
 <Navbar title="TextUtils" aboutText="About Us" />
+<div className="container my-3">
+
+  <TextForm heading="Enter the text to analyze" />
+
+</div>
+
    </>
   );
 }
