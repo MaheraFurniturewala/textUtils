73c0f4522a710b763b9e232bf6747eaee087f3c9
Mahera Furniturewala <maherafurniturewala1111@gmail.com>
2021-09-22 02:05:19 +0530

    changes in About.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import React,{useState} from 'react';  ///imrs
 import './App.css';
 import Navbar from './components/Navbar.js';
 import TextForm from './components/TextForm.js';
 import About from './components/About.js';
 function App() {
+  const [mode, setMode] = useState('light'); //whether dark mode is enabled or not
   return (
    <>
    {/* here the title is a prop that is being passed*/}
-<Navbar title="TextUtils" aboutText="About Us" />
+<Navbar title="TextUtils" aboutText="About Us" mode={mode} />
 <div className="container my-3">
 
   <TextForm heading="Enter the text to analyze below" />
